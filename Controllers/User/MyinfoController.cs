a8256a81b71c07786da6b5ad1b849532cdc695de
mr77777777 <1303398957@qq.com>
2024-08-23 17:14:50 +0800

    add 个人中心的后端
    尚未确认是否可行

---
+++ Controllers/User/MyinfoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetTaste;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using UniTrade.Models;
+using UniTrade.Tools;
+using UniTrade.ViewModels;
+
+
+
+
+namespace UniTrade.Controllers.User
+{
+    [Route("getMyinfo")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> GetMyinfo([FromBody] MyinfoViewModel query)
+        {
+            SqlSugarClient db = Database.GetInstance();
+            try
+            {
+                var user = db.Queryable<USERS>()
+                    .Where(a => a.USER_ID == query.ID)
+                    .First();
+                if(user == null)
+                {
+                    return Unauthorized("用户不存在");   /*暂定，用于测试，实际上线后按照*/
+                }
+                var token = JwtService.GenerateAccessToken(user.USER_ID,"");
+                var back = new { token = token,
+                    id = user.USER_ID,name = user.NAME,avatar=user.AVATAR,
+                    address = user.ADDRESS,phone = user.PHONE,reputation = user.REPUTATION,
+                    sex=user.SEX};
+                return (Ok(back));
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+
+}
