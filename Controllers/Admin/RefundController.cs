62c51f471c1eb6dde94870e3656116c6a7de391e
flameagain <3110630186@qq.com>
2024-08-26 18:04:48 +0800

    feat ：退款功能后端

---
+++ Controllers/Admin/RefundController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using SqlSugar;
+using System;
+using UniTrade.Models;
+using UniTrade.ViewModels;
+using UniTrade.Tools;
+
+namespace UniTrade.Controllers
+{
+    [Route("refund")]
+    [ApiController]
+    public class RefundController : ControllerBase
+    {
+        // 获取所有退款信息
+        [HttpGet("query")]
+        public IActionResult GetRefunds()
+        {
+            SqlSugarClient db = Database.GetInstance();
+            try
+            {
+                var refunds = db.Queryable<REFUNDS, REFUND_ON, HOLDS, PLACES, USERS, USERS>(
+                        (r, ro, h, p, seller, buyer) => new object[] {
+                            JoinType.Inner, r.REFUND_ID == ro.REFUND_ID,   // REFUNDS 与 REFUND_ON 表连接
+                            JoinType.Inner, ro.ORDER_ID == h.ORDER_ID,    // REFUND_ON 与 HOLDS 表连接
+                            JoinType.Inner, ro.ORDER_ID == p.ORDER_ID,    // REFUND_ON 与 PLACES 表连接
+                            JoinType.Inner, h.SELLER_ID == seller.USER_ID,   // HOLDS 与 USERS 表连接
+                            JoinType.Inner, p.CUSTOMER_ID == buyer.USER_ID      // PLACES 与 USERS 表连接
+                        })
+                        .Where(r => r.REFUND_STATE == "Pending") // 仅获取状态为 "Pending" 的退款记录
+                        .Select((r, ro, h, p, seller, buyer) => new Refundinfo
+                        {
+                            seller_id = h.SELLER_ID,
+                            seller_name = seller.NAME,
+                            buyer_id = p.CUSTOMER_ID,
+                            buyer_name = buyer.NAME,
+                            // Commodity 信息
+                            reason = r.REFUND_REASON,
+                            time = r.REFUND_TIME,
+                        })
+                        .ToList();
+                return Ok(refunds);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ViewModels/Refundinfo.cs
@@ -0,0 +1,12 @@
+﻿namespace UniTrade.ViewModels
+{
+    public class Refundinfo
+    {
+        public string seller_id { get; set; }
+        public string seller_name { get; set; }
+        public string buyer_id { get; set; }
+        public string buyer_name { get; set; }
+        public string reason { get; set; }
+        public System.DateTime time { get; set; }
+    }
+}
