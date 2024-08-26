edba0d8cded1ffc6030b73653e9f6e792a196c82
CChris152 <1650306859@qq.com>
2024-08-27 00:53:12 +0800

    add 管理员验证请求

---
+++ Controllers/Admin/AdminJudgeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using SqlSugar;
+using System;
+using UniTrade.Models;
+using UniTrade.ViewModels;
+using UniTrade.Tools;
+using System.Security.Claims;
+
+namespace UniTrade.Controllers.Admin
+{
+    [Route("adminjudge")]
+    [ApiController]
+    public class AdminJudgeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CheckToken()
+        {
+            var userRole = HttpContext.User.FindFirstValue(ClaimTypes.Role);
+            if(userRole== "Admin")
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Unauthorized(false);
+            }
+        }
+    }
+}
