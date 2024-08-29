fb09d9934fada4d0223b815a29a5f6552e7461ff
flameagain <3110630186@qq.com>
2024-08-30 00:22:43 +0800

    feat： 增加审核退款，并修改查询退款

--- ViewModels/Refundinfo.cs
+++ ViewModels/Refundinfo.cs
@@ -1,12 +1,19 @@
 ﻿namespace UniTrade.ViewModels
 {
-    public class Refundinfo
+    public class QueryRefundInfo
     {
         public string seller_id { get; set; }
         public string seller_name { get; set; }
         public string buyer_id { get; set; }
         public string buyer_name { get; set; }
+        public string merchandise_name { get; set; }
         public string reason { get; set; }
         public System.DateTime time { get; set; }
     }
+
+    public class AuditRefundInfo
+    {
+        public string refund_id { get; set; }
+    }
+
 }
