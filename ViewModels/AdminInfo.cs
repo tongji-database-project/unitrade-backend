34054bc12ed40398616f3d4289c62dd10c149182
CChris152 <1650306859@qq.com>
2024-08-30 15:29:28 +0800

    feat 管理员信息API

---
+++ ViewModels/AdminInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace UniTrade.ViewModels
+{
+    public class AdminInfo
+    {
+        public string admin_id { get; set; }
+        public string admin_name { get; set; }
+        public bool admin_level { get; set; }
+    }
+}
