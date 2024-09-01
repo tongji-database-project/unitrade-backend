5440917c43a00f18ab2a9eff5a074c5ebec18562
mr77777777 <1303398957@qq.com>
2024-09-01 19:02:12 +0800

    add 得到所有订单、已收货、运输中、未评论订单的数量后端

---
+++ ViewModels/OrderNumViewModels.cs
@@ -0,0 +1,12 @@
+﻿namespace UniTrade.ViewModels
+{
+    public class OrderNumViewModels
+    {
+        public int ORDER_NUM { get; set; }
+        public int RECIEVED_NUM { get; set; }
+
+        public int TRANSIT_NUM { get; set; }
+
+        public int UNCOMMAND_NUM { get; set; }
+    }
+}
