646aebe169d0cc037b342aaff24b4ec94fe76055
CChris152 <1650306859@qq.com>
2024-09-06 11:19:57 +0800

    fix 下架商品功能后端框架

---
+++ ViewModels/MerchandiseAuditInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace UniTrade.ViewModels
+{
+    public class MerchandiseAuditInfo
+    {
+        string merchandise_id { get; set; }
+        string merchandise_name { get; set; }
+        string merchandise_type { get; set; }
+        decimal merchandise_price { get; set; }
+        string seller_name { get; set; }
+        short reputation { get; set; }
+        int commit_sum { get; set; }
+        float average_point { get; set; }
+    }
+
+    public class MerchandiseCommit
+    {
+        string merchandise_id { get; set; }
+        int point { get; set; }
+    }
+
+    public class MerchandiseCommitStatistics
+    {
+        string merchandise_id { get; set; }
+        int sum { get; set; }
+        float average { get; set; }
+    }
+}
